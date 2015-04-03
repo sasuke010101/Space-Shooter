cbf6a44faff55bb696b03c80e7f8e65324f07664
Wu Haoxiang <WuHaoxiang@WudeMacBook-Pro.local>
2015-04-04 00:43:49 +0800

    add sound to the explosion and GameController to control the waves

---
+++ Space-Shooter/Assets/Scripts/DestroyByTime.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour 
+{
+	void Start()
+	{
+		Destroy (gameObject, 2);
+	}
+	
+}
---
+++ Space-Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour 
+{
+	public GameObject hazard;
+	public Vector3 spawnValues;
+	public int hazarCount;
+	public float spawnWait;
+	public float startWait;
+	public float waveWait;
+
+	void Start()
+	{
+		StartCoroutine (spawnWaves ());
+	}
+
+	IEnumerator spawnWaves()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true) {
+			for (int i = 0; i < hazarCount; i++) 
+			{
+				Vector3 spawnPostion = new Vector3 (Random.Range(-spawnValues.x,spawnValues.x),spawnValues.y,spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (hazard, spawnPostion, spawnRotation);
+				yield return new WaitForSeconds(spawnWait);
+			}
+			yield return new WaitForSeconds(waveWait);
+		}
+	}
+}
